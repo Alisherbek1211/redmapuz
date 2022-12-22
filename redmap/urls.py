1b612f14149024fddb240017cd8323f04d07656c
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2022-12-22 16:57:44 +0500

    test

--- redmap/urls.py
+++ redmap/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import nature
+from .views import nature , regionListView
 urlpatterns = [
-    path('',nature)
+    path('',nature),
+    path("regions/",regionListView,name ="region"),
 ]

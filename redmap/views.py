57c3845344bac081e11d127d5ac637b522d328d9
Alisherbek <alisheryuldashev1211@gmail.com>
2023-02-27 15:46:09 +0500

    dd

--- redmap/views.py
+++ redmap/views.py
@@ -6,7 +6,7 @@ import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .ser import Hayvonser,Osimlikser
-model = SentenceTransformer('clip-ViT-B-32')
+# model = SentenceTransformer('clip-ViT-B-32')
 from django.http import Http404
 from .forms import Getimage
 
